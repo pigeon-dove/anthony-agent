8c681a6938ead19ffa388f8c7e3a942e3cbb59aa
anthonyxue <anthonyxue@tencent.com>
2026-03-18 21:14:03 +0800

    feat: support bash background

--- src/tools/builtins/__init__.py
+++ src/tools/builtins/__init__.py
@@ -3,9 +3,13 @@ from src.tools.builtins.write_file import WriteFileTool
 from src.tools.builtins.edit_file import EditFileTool
 from src.tools.builtins.multi_edit import MultiEditTool
 from src.tools.builtins.bash import BashTool
+from src.tools.builtins.bash_background import BackgroundBashTool
 from src.tools.builtins.ls import LsTool
 from src.tools.builtins.glob import GlobTool
 from src.tools.builtins.grep import GrepTool
 
 # 导出所有内置工具类
-BUILTIN_TOOLS = [ReadFileTool, WriteFileTool, EditFileTool, MultiEditTool, BashTool, LsTool, GlobTool, GrepTool]
+BUILTIN_TOOLS = [
+    ReadFileTool, WriteFileTool, EditFileTool, MultiEditTool,
+    BashTool, BackgroundBashTool, LsTool, GlobTool, GrepTool,
+]
